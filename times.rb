f43bd29342f0ecf0919d23188f2f6b6fa83a2ccc
Charles Pelton <charliejp0311@gmail.com>
2020-01-21 19:32:32 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -1,13 +1,10 @@
 def using_times
 	#your code here
-	phrase = ""
 	7.times do 
-	  phrase += "Wingardium Leviosa"
+	  puts "Wingardium Leviosa"
 	end
-	phrase
-	#binding.pry 
 end
 
-
+using_times
 
 
